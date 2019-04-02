a85974afb307b9920459f9c799b2c4aa82a4e367
Christian MilNeil <cmilneil@Christians-MacBook-Pro.local>
2019-04-02 09:53:04 -0400

    Added transitions to animate trade routes

--- map.js
+++ map.js
@@ -2,7 +2,7 @@ var canvas, pathGenerator, width, height;
 
 var zoomedOut = false, rotatemap, mapTop, mineralListHeight; // to be defined when DOM loads or window size changes, for scroll events
 
-var projection = d3.geoEckert3();
+var projection = d3.geoEckert3(), projection2 = d3.geoEckert3();
 
 const chinaCoord = [103.8,37];
 
@@ -53,191 +53,211 @@ const nations = d3.json("https://unpkg.com/world-atlas@1/world/50m.json")
   	
   	geodata = topojson.simplify( topojson.presimplify(geodata), 0.02 );
   	
-		mapTop = document.getElementById("map").getBoundingClientRect().top;
-		mineralListHeight = document.getElementById("map").getBoundingClientRect().height;
-		 
-		scrollInit();
-  	
-  	width = d3.select("#map").node().getBoundingClientRect().width;
-  	height = width * 0.5;
-
-	 	svg = d3.select('#map').append('svg')
-	 		.attr('width', width)
-      .attr('height', height);
-	  
 		var graticule = d3.geoGraticule();
 
-	  projection
-	  	.rotate([-105, -35])
-	  	.fitSize([width, height*1.1], {
-		    type: "Sphere"
-		  })
-      .translate([width/2, height/2 + 10])
-	    .scale(width / 1.2);
+		svg = d3.select('#map').append('svg');
 
-	  pathGenerator = d3.geoPath().projection(projection);
+		currentProjection = projection;
+
+		drawMaps();
 	  
-		let nations = svg.append('g').attr("id","nationPaths").selectAll("path.nation")
-			.data(topojson.feature(geodata, geodata.objects.countries).features)
-			.enter().append('path')
-			.attr("d", pathGenerator)
-			.attr('opacity',1)
-			.style("stroke", d => (d.id == "156") ? "rgba(255,0,0,0.5)" : null ) 
-			.style("stroke-width", d => (d.id == "156") ? "1.5px" : null ) 
-			.attr("class", d => {
-				let thisPlace = findElement(tradeData, "id", d.id);
-				if (thisPlace) { 
-					return "nation land highlight " + thisPlace.info.element.replace(/\s+/g, '').toLowerCase();
-				} 
-				else { return "nation land" }
-			})
-			.lower();
+		function drawMaps() {
 
-		d3.selectAll('path.highlight')
-			.on('mouseover', showInfo)
-			.on('mouseout', hideInfo);
+			mapTop = document.getElementById("map").getBoundingClientRect().top;
+			mineralListHeight = document.getElementById("map").getBoundingClientRect().height;
+			 
+			scrollInit();
+	  	
+	  	width = d3.select("#map").node().getBoundingClientRect().width;
+	  	height = width * 0.5;
+		 	
+		 	svg.attr('width', width)
+	      .attr('height', height);
 
-		svg.append("path")
-	    .datum({ type: "Sphere" })
-	    .attr("d", pathGenerator)
-	    .classed("sphere", true)
-	    .lower();
+	    svg.selectAll('*').remove();
 
-	  svg.append("path")
-	    .datum(graticule)
-	    .attr("class", "graticule")
-	    .attr('fill','none').attr('stroke','#ddd')
-	    .attr("d", pathGenerator)
-	    .lower();
+			projection
+		  	.fitSize([width, height*1.1], {
+			    type: "Sphere"
+			  })
+	      .translate([width/2, height/2 + 10])
+		    .rotate([-105, -35])
+		  	.scale(width / 1.2);
 
+		  projection2
+		  	.fitSize([width, height*1.1], {
+			    type: "Sphere"
+			  })
+	      .translate([width/2, height/2 + 10])
+		    .scale(width/6).rotate([-5,0]);
 
-	  
-	function showInfo(item) {
-		
-		let c = findElement(tradeData, "id", item.id);
-		
-		let ib_w = document.getElementById("mapInfoBox").getBoundingClientRect().width;
-		let c_x = pathGenerator.centroid(item)[0], c_y = pathGenerator.centroid(item)[1];
-		let ib_x = (c_x + ib_w > width) ? c_x - ib_w - 20 : c_x + 20 ;
+		  pathGenerator = d3.geoPath().projection(currentProjection);
+	
+			let nations = svg.append('g').attr("id","nationPaths").selectAll("path.nation")
+				.data(topojson.feature(geodata, geodata.objects.countries).features)
+				.enter().append('path')
+				.attr("d", pathGenerator)
+				.attr('opacity',1)
+				.style("stroke", d => (d.id == "156") ? "rgba(255,0,0,0.5)" : null ) 
+				.style("stroke-width", d => (d.id == "156") ? "1.5px" : null ) 
+				.attr("class", d => {
+					let thisPlace = findElement(tradeData, "id", d.id);
+					if (thisPlace) { 
+						return "nation land highlight " + thisPlace.info.element.replace(/\s+/g, '').toLowerCase();
+					} 
+					else { return "nation land" }
+				})
+				.lower();
 
-		let ib = document.getElementById("mapInfoBox");
+			d3.selectAll('path.highlight')
+				.on('mouseover', showInfo)
+				.on('mouseout', hideInfo);
 
-		ib.style.top = c_y + 'px';
-		ib.style.left = ib_x + 'px';
-		
+			var arcs = svg.append("g").selectAll('path.arc').data( tradeData, JSON.stringify );
 
-		d3.select("#infoBox_Country").html(c.name);
-		d3.select("#infoBox_Mineral")
-			.attr('class' , c.info.element.replace(/\s+/g, '').toLowerCase())
-			.html(c.info.element);
-		d3.select("#infoBox_Production").html(c.info.production);
-		d3.select("#infoBox_Reserves").html(c.info.reserves);
+		  arcs.enter()
+		    .append('path')
+		    .attr('class', d => 'arc ' + d.info.element.replace(/\s+/g, '').toLowerCase() )
+		    .attr('d', d =>  drawCurve(projection2(chinaCoord), projection2([d.lng, d.lat]), projection2(d.mid) ) )
+		    .attr('opacity',0)
 
-		d3.select("#infoBox_Chatter").html(c.info.chatter);
+			svg.append("path")
+		    .datum({ type: "Sphere" })
+		    .attr("d", pathGenerator)
+		    .classed("sphere", true)
+		    .lower();
 
-		ib.classList.remove('hidden');
-	}
+		  svg.append("path")
+		    .datum(graticule)
+		    .attr("class", "graticule")
+		    .attr('fill','none').attr('stroke','#ddd')
+		    .attr("d", pathGenerator)
+		    .lower();
 
-	function hideInfo(item) {
-		let ib = document.getElementById("mapInfoBox");
+		  var arcs = svg.append("g").selectAll('path.arc').data( tradeData, JSON.stringify );
 
-		ib.classList.add('hidden');
-	}
+		  arcs.enter()
+		    .append('path')
+		    .attr('class', d => 'arc ' + d.info.element.replace(/\s+/g, '').toLowerCase() )
+		    .attr('d', d =>  drawCurve(projection2(chinaCoord), projection2([d.lng, d.lat]), projection2(d.mid) ) )
+		    .attr('opacity',0);
+		}
 
-	function rotateMaps() {
+		function showInfo(item) {
+			
+			let c = findElement(tradeData, "id", item.id);
+			
+			let ib_w = document.getElementById("mapInfoBox").getBoundingClientRect().width;
+			let c_x = pathGenerator.centroid(item)[0], c_y = pathGenerator.centroid(item)[1];
+			let ib_x = (c_x + ib_w > width) ? c_x - ib_w - 20 : c_x + 20 ;
 
-		var nextRotationDestination = [-5,0];
+			let ib = document.getElementById("mapInfoBox");
 
-		// drawarcs();
+			ib.style.top = c_y + 'px';
+			ib.style.left = ib_x + 'px';
+			
 
-		var arcs = svg.append("g").selectAll('path.arc').data( tradeData, JSON.stringify );
+			d3.select("#infoBox_Country").html(c.name);
+			d3.select("#infoBox_Mineral")
+				.attr('class' , c.info.element.replace(/\s+/g, '').toLowerCase())
+				.html(c.info.element);
+			d3.select("#infoBox_Production").html(c.info.production);
+			d3.select("#infoBox_Reserves").html(c.info.reserves);
 
-	  arcs.enter()
-	    .append('path')
-	    .attr('class', d => 'arc ' + d.info.element.replace(/\s+/g, '').toLowerCase() )
-	    .attr('d', d =>  drawCurve(projection(chinaCoord), projection([d.lng, d.lat]), projection(d.mid) ) )
-	    .attr('opacity',0)
-	    
+			d3.select("#infoBox_Chatter").html(c.info.chatter);
 
-		svg
-			.selectAll("path")
-			.transition()
-			.duration(2500).delay(500)
-			.attr('opacity',1)	 
-			.style("stroke-width", null ) 
-			.attrTween("d", function(geoJsonFeature) {
-	      // for the current element being transitioned, figure out which svg "map" it belongs to
-	      // this needs to happen because each svg "map" uses a different projection and geoPathGenerator
-	      var geoPathGenerator = pathGenerator;
+			ib.classList.remove('hidden');
+		}
 
-	      var rotateInterpolator = d3.interpolate(
-	        geoPathGenerator.projection().rotate(),
-	        nextRotationDestination
-	      );
+		function hideInfo(item) {
+			let ib = document.getElementById("mapInfoBox");
 
-	      var scaleInterpolator = d3.interpolate(
-	        geoPathGenerator.projection().scale(),
-	        width/5.2
-	      );
+			ib.classList.add('hidden');
+		}
 
-	      // find out if the landmasses or one of the circles is the element currently being transitioned
-	      if ( (d3.select(this).classed("land")) || (d3.select(this).classed("graticule")) ) {
-	        return function(t) {
-	          geoPathGenerator.projection()
-	          	.rotate(rotateInterpolator(t))
-	          	.scale(scaleInterpolator(t));
+		function rotateMaps() {
 
-	          return geoPathGenerator(geoJsonFeature) || "";
-	        }
-	      } else if (d3.select(this).classed("arc")) {
-	      		
-	      		let d = d3.select(this).datum();
-	      		return function(t) {
-	      			let p = projection
-		          	.rotate(rotateInterpolator(t))
-		          	.scale(scaleInterpolator(t));
+			currentProjection = projection2;
 
-		          return drawCurve(p(chinaCoord) , p([d.lng, d.lat]), p(d.mid));
-	      		}
-	      } else {
-	        return "";
-	      }
-	    });
+			svg
+				.selectAll("path")
+				.transition()
+				.duration(6000).delay(500)
+				.attr('opacity',1)	 
+				.style("stroke-width", null ) 
+				.attrTween("stroke-dasharray", tweenDash)
+				.attrTween("d", function(geoJsonFeature) {
+		      // for the current element being transitioned, figure out which svg "map" it belongs to
+		      // this needs to happen because each svg "map" uses a different projection and geoPathGenerator
+		      var geoPathGenerator = pathGenerator;
 
-	    zoomedOut = true;
+		      var rotateInterpolator = d3.interpolate(
+		        projection.rotate(),
+		        projection2.rotate()
+		      );
 
-	}
+		      var scaleInterpolator = d3.interpolate(
+		        projection.scale(),
+		      	projection2.scale()
+		      );
 
-	function resize() {
-		mapTop = document.getElementById("map").getBoundingClientRect().top;
-		mineralListHeight = document.getElementById("map").getBoundingClientRect().height;
-		
-		newWidth = window.innerWidth, scale = newWidth/width;
+		      // find out if the landmasses or one of the circles is the element currently being transitioned
+		      if ( (d3.select(this).classed("land")) || (d3.select(this).classed("graticule")) ) {
+		        return function(t) {
 
-		svg.attr('transform','translate(' + (newWidth-width)/2 + ',0)scale(' + scale + ')' );
+		        	// t goes from 0 to 1
 
-		width = newWidth;
+		          geoPathGenerator.projection()
+		          	.rotate(rotateInterpolator(Math.min(1,t*3)))
+		          	.scale(scaleInterpolator(Math.min(1,t*3)));
 
-		scrollInit() ;
-	}
+		          return geoPathGenerator(geoJsonFeature) || "";
+		        }
+		      } else if (d3.select(this).classed("arc")) {
+		      		
+		      		let d = d3.select(this).datum();
 
-	function scrollInit() {
+		      		return function(t) {
+		      			let p = projection
+			          	.rotate(rotateInterpolator(Math.min(1,t*3)))
+		          		.scale(scaleInterpolator(Math.min(1,t*3)));
 
-		document.getElementById("section2").style.height= window.innerHeight*1.8 + 'px';	
+			          return drawCurve(p(chinaCoord) , p([d.lng, d.lat]), p(d.mid));
+		      		}
+		      } else {
+		        return "";
+		      }
+		    });
 
-		window.addEventListener('scroll', function(e) {    // event triggers on scrolling
-		    var current = window.scrollY;
-		    //console.log('y',current); 
-		    if ((current > (mapTop + mineralListHeight)) && !zoomedOut) {
-		    	rotateMaps();
-		    }
-		});
-	}
+				function tweenDash() {
+					if (d3.select(this).classed("arc")) {
+						var l = this.getTotalLength(),
+						    i = d3.interpolateString("0," + l, l + "," + l);
+						return function (t) { return i(t); };
+					} else { return ""; }
+				}
 
-	window.onresize = resize;
+		    zoomedOut = true;
 
+		}
+
+		function scrollInit() {
+
+			document.getElementById("section2").style.height= window.innerHeight*1.8 + 'px';	
+
+			window.addEventListener('scroll', function(e) {    // event triggers on scrolling
+			    var current = window.scrollY;
+			    //console.log('y',current); 
+			    if ((current > (mapTop + mineralListHeight)) && !zoomedOut) {
+			    	rotateMaps();
+			    }
+			});
+		}
+
+		window.addEventListener("resize", drawMaps);
  
 });
+
 
 
 function drawCurve(origin, dest, mid) {
@@ -252,9 +272,6 @@ function drawCurve(origin, dest, mid) {
 	 return pathstr;
 }
 
-function addHtml(content) {
-	return document.createTextNode(content);
-}
 
 function findElement(arr, propName, propValue) {
   var returnVal = false;
